40e58b7f2683d979be1a44317fa1786ac163cc74
Eoin <eahern123@gmail.com>
2020-12-29 15:15:40 +0000

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "Nocturnal"
+    architecture "x64"
+
+    configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    
+project "Nocturnal"
+    location "Nocturnal"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+
+    files{
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs{
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines{
+            "NOC_PLATFORM_WINDOWS",
+            "NOC_BUILD_DLL"
+        }
+    postbuildcommands{
+        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+
+    filter "configurations:Debug"
+		defines "NOC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NOC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NOC_DIST"
+		optimize "On"
+
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Nocturnal/vendor/spdlog/include",
+		"Nocturnal/src"
+	}
+
+	links
+	{
+		"Nocturnal"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"NOC_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "NOC_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "NOC_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "NOC_DIST"
+		optimize "On" 
